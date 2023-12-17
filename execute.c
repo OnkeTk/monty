f125fe0cf2c1130ff3f7d33c5891a7f796794c2f
OnkeTk <onkemetsetikane@gmail.com>
2023-12-17 15:12:00 -0800

    executes the opcode

--- execute.c
+++ execute.c
@@ -10,21 +10,21 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-				{"pop", f_pop},
-				{"swap", f_swap},
-				{"add", f_add},
-				{"nop", f_nop},
-				{"sub", f_sub},
-				{"div", f_div},
-				{"mul", f_mul},
-				{"mod", f_mod},
-				{"pchar", f_pchar},
-				{"pstr", f_pstr},
-				{"rotl", f_rotl},
-				{"rotr", f_rotr},
-				{"queue", f_queue},
-				{"stack", f_stack},
+				{"push", func_push}, {"pall", func_pall}, {"pint", func_pint},
+				{"pop", func_pop},
+				{"swap", func_swap},
+				{"add", func_add},
+				{"nop", func_nop},
+				{"sub", func_sub},
+				{"div", func_div},
+				{"mul", func_mul},
+				{"mod", func_mod},
+				{"pchar", func_pchar},
+				{"pstr", func_pstr},
+				{"rotl", func_rotl},
+				{"rotr", func_rotr},
+				{"queue", func_queue},
+				{"stack", func_stack},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
